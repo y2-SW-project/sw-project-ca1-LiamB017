82f4edf67716126fb38b0479be021ff773658ab2
LiamB017 <liambedford17@gmail.com>
2022-02-25 13:57:48 +0000

    Customised Bootstrap

--- MuayTrading/resources/views/welcome.blade.php
+++ MuayTrading/resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
     <title>lol</title>
 
 </head>
@@ -30,7 +32,7 @@
                 <a class="navbar-brand" href="#">Navbar</a>
                 <a class="navbar-brand" href="#">ha</a>
                 <a class="navbar-brand" href="#">Navbar</a>
-
+ <h1>TESTING HYEON</h1>
 
             </div>
     </div>
